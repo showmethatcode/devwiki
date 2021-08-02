86db9eb5c55f3665c0599e09c16ff0cb2fd8f4ee
iam-dante <koreanhoseon@gmail.com>
2021-08-02 19:55:04 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,24 +2,20 @@ const express = require('express');
 const cors = require('cors')
 const morgan = require("morgan");
 const logger = morgan("dev");
-const { router } = require('./rest/routes')
+const { router } = require('./rest/routes');
 
-// function createApplication(prisma) {
-//   const app = express();
-//   app.use(express.urlencoded({ extended: true }));
-//   app.use(express.json());
-//   app.use(cors());
-//   app.use(logger);
-//   app.use((req, res, next) => {
-//     req.context = {
-//       prisma,
-//       req,
-//       res
-//     }
-//     next();
-//   })
-//   app.use(router)
-//   return app
-// }
+function createApplication(prisma) {
+  const app = express();
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(cors());
+  app.use(logger);
+  app.use((req, res, next) => {
+    req.context = {req, res, prisma};
+    next();
+  });
+  app.use(router);
+  return app
+}
 
-// module.exports = createApplication
+module.exports = createApplication
