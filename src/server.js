dcf013f104d3f3129b26095d8d6a9eee7ca15e7c
iam-dante <koreanhoseon@gmail.com>
2021-08-04 23:03:36 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
-const { PrismaClient } = require('@prisma/client');
-const createApplication = require('./app');
-const PORT = process.env.PORT
+const { PrismaClient } = require("@prisma/client");
+const createApplication = require("./app");
+const PORT = process.env.PORT;
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 function handleListening() {
-  const app = createApplication(prisma)
+  const app = createApplication(prisma);
   app.listen(PORT, () => {
-    console.log('✅ Sever Listening on port http://localhost:3000/')
-  })
+    console.log("✅ Sever Listening on port http://localhost:3000/");
+  });
 }
 
-handleListening()
+handleListening();
