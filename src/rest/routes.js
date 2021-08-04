074b13387ef5e5f7ab74d40f7f4dad758e4c8df7
iam-dante <koreanhoseon@gmail.com>
2021-08-04 23:03:52 +0900

    Add detail router

--- src/rest/routes.js
+++ src/rest/routes.js
@@ -1,9 +1,14 @@
-const { Router } = require('express');
-const { home, write } = require('./controllers/termController')
+const { Router } = require("express");
+const {
+  home,
+  termCreate,
+  termDetail,
+} = require("./controllers/termController");
 
 const router = Router();
 
-router.get('/', home)
-router.post('/terms/write', write)
+router.get("/", home);
+router.post("/terms/write", termCreate);
+router.get("/terms/:id", termDetail);
 
-module.exports = { router } 
+module.exports = router;
