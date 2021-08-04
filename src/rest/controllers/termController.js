2722ac009320e33b366c8468ec67be5639fb5fa2
iam-dante <koreanhoseon@gmail.com>
2021-08-04 23:04:04 +0900

    Update termController.js

--- src/rest/controllers/termController.js
+++ src/rest/controllers/termController.js
@@ -1,43 +1,66 @@
+const { prisma } = require("@prisma/client");
+
 const home = async (req, res) => {
-  const { prisma } = req.context
-  let is10Newest = []
+  const { prisma } = req.context;
+  let is10Newest = [];
 
   const terms = await prisma.term.findMany({
     take: 10,
     orderBy: {
-      createdAt: "desc"
-    }
-  })
+      createdAt: "desc",
+    },
+  });
 
   for (let i = 0; i < terms.length; i++) {
-    is10Newest.push(terms[i].name)
+    is10Newest.push(terms[i].name);
   }
 
   res.send({
-    ...is10Newest
-  })
-}
+    ...is10Newest,
+  });
+};
 
-const write = async (req, res) => {
-  const { prisma } = req.context
+const termCreate = async (req, res) => {
+  const { prisma } = req.context;
   const { name, description } = req.body;
 
   const createdTerm = await prisma.term.create({
     data: {
       name,
-      posts: {
+      termRevision: {
         create: {
-          description
-        }
-      }
-    }
-  })
+          description,
+        },
+      },
+    },
+    include: { termRevision: true },
+  });
+
+  await prisma.termPointer.create({
+    data: {
+      termId: createdTerm.id,
+      termRevisionId: createdTerm.termRevision[0].id,
+    },
+  });
+
   res.send({
     ...createdTerm,
-  })
-}
+  });
+};
+
+const termDetail = async (req, res) => {
+  const { prisma } = req.context;
+  const { id } = req.params;
+  id = Number(id);
+  const findTerm = await prisma.term.findUnique({
+    where: {
+      id: id,
+    },
+  });
+};
 
 module.exports = {
   home,
-  write
-}
+  termCreate,
+  termDetail,
+};
