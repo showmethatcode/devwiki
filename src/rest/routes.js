ee1c5e02e0bfa04e3f69e5c35bd96f57dcb966a4
iam-dante <koreanhoseon@gmail.com>
2021-08-06 20:40:22 +0900

    Update variable name

--- src/rest/routes.js
+++ src/rest/routes.js
@@ -1,14 +1,17 @@
 const { Router } = require("express");
 const {
   home,
-  termCreate,
-  termDetail,
+  writeTerm,
+  detailTerm,
+  getUpdateTerm,
+  postUpdateTerm,
 } = require("./controllers/termController");
 
 const router = Router();
 
 router.get("/", home);
-router.post("/terms/write", termCreate);
-router.get("/terms/:id", termDetail);
+router.post("/terms/write", writeTerm);
+router.route("/terms/:id/edit").get(getUpdateTerm).post(postUpdateTerm);
+router.get("/terms/:id", detailTerm);
 
 module.exports = router;
