35dcf38a7b6e7a81c3d58ef22441bf49a734b4cb
iam-dante <koreanhoseon@gmail.com>
2021-08-09 23:35:31 +0900

    Change variable naming

--- src/rest/controllers/termController.js
+++ src/rest/controllers/termController.js
@@ -21,12 +21,11 @@ const home = async (req, res) => {
   });
 };
 
-const writeTerm = async (req, res) => {
+const createTerm = async (req, res) => {
   const { prisma } = req.context;
   const { name, description } = req.body;
 
-  const createTerm = await prisma.term.create({
-    // 용어 생성과 동시에 내용 생성, termRevision 정보도 가져온다.
+  const newTerm = await prisma.term.create({
     data: {
       name,
       termRevision: {
@@ -38,17 +37,16 @@ const writeTerm = async (req, res) => {
     include: { termRevision: true },
   });
 
-  const createTermPointer = await prisma.termPointer.create({
-    // 위에서 가져온 termRevision으로 termPointer에 있는 termRevisionId value 저장
+  const newPointer = await prisma.termPointer.create({
     data: {
-      termId: createTerm.id,
-      termRevisionId: createTerm.termRevision[0].id,
+      termId: newTerm.id,
+      termRevisionId: newTerm.termRevision[0].id,
     },
   });
 
   res.send({
-    ...createTerm,
-    ...createTermPointer,
+    ...newTerm,
+    ...newPointer,
   });
 };
 
@@ -57,27 +55,22 @@ const getUpdateTerm = async (req, res) => {
   const id = Number(req.params.id);
 
   const term = await prisma.term.findUnique({
-    // 업데이트 할 용어 가져오고, termPointer 정보도 가져온다.
     where: {
       id,
     },
     include: { termPointer: true },
   });
 
-  const revisions = await prisma.termRevision.findMany({
-    // term에서 가져온 termPointer 정보에 담겨있는 termRevisionId 값을 이용해 체크포인트로 저장된 description을 보내줌
+  const termRevision = await prisma.termRevision.findMany({
     where: {
       id: term.termPointer.termRevisionId,
       termId: id,
     },
   });
 
-  const termName = term.name;
-  const termDescription = revisions[0].description;
-
   res.send({
-    termName,
-    termDescription,
+    term,
+    termRevision,
   });
 };
 
@@ -86,8 +79,7 @@ const postUpdateTerm = async (req, res) => {
   const id = Number(req.params.id);
   const { description } = req.body;
 
-  const createDescription = await prisma.termRevision.create({
-    // 같은 용어를 다루기에 params에서 id값 받아와 termId에 넣어준 후 request body에서 description 받아와 새로운 튜플 생성, 로그를 남겨야 하기에 update가 아닌 create 함
+  const storeDescription = await prisma.termRevision.create({
     data: {
       description,
       termId: id,
@@ -95,17 +87,16 @@ const postUpdateTerm = async (req, res) => {
   });
 
   const updateTermPointer = await prisma.termPointer.update({
-    // termPointer는 현재 어떤 description을 사용하는지를 알 수 있는 체크포인트이기에 termRevisionId만 update 함
     where: {
       termId: id,
     },
     data: {
-      termRevisionId: createDescription.id,
+      termRevisionId: storeDescription.id,
     },
   });
 
   res.send({
-    ...createDescription,
+    ...storeDescription,
     ...updateTermPointer,
   });
 };
@@ -121,26 +112,22 @@ const detailTerm = async (req, res) => {
     include: { termPointer: true },
   });
 
-  const revisions = await prisma.termRevision.findMany({
-    // findTerm에서 가져온 termPointer 정보에 담겨있는 termRevisionId 값을 이용해 체크포인트로 저장된 description을 보내줌
+  const termRevision = await prisma.termRevision.findMany({
     where: {
       id: term.termPointer.termRevisionId,
       termId: id,
     },
   });
 
-  const termName = term.name;
-  const termDescription = revisions[0].description;
-
   res.send({
-    termName,
-    termDescription,
+    term,
+    termRevision,
   });
 };
 
 module.exports = {
   home,
-  writeTerm,
+  createTerm,
   detailTerm,
   getUpdateTerm,
   postUpdateTerm,
