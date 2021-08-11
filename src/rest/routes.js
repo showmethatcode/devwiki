64ffd8c82eef85e2eb302f392c280ef2ccebf019
iam-dante <koreanhoseon@gmail.com>
2021-08-11 18:40:11 +0900

    Update routes.js
    컨트롤러 째서 다시 할당함

--- src/rest/routes.js
+++ src/rest/routes.js
@@ -1,15 +1,15 @@
 const { Router } = require("express");
+const getRecentTermList = require("./controllers/terms/home");
+const createTerm = require("./controllers/terms/createTerm");
+const detailTerm = require("./controllers/terms/detailTerm");
 const {
-  home,
-  createTerm,
-  detailTerm,
   getUpdateTerm,
   postUpdateTerm,
-} = require("./controllers/termController");
+} = require("./controllers/terms/updateTerm");
 
 const router = Router();
 
-router.get("/terms/recent", home);
+router.get("/terms/recent", getRecentTermList);
 router.post("/terms/write", createTerm);
 router.route("/terms/:id/edit").get(getUpdateTerm).post(postUpdateTerm);
 router.get("/terms/:id", detailTerm);
