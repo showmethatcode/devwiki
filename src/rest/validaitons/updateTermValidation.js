4d02791cc7860474f341dd1c3787fcc83e99ade6
well-balanced <iam.woosik.kim@gmail.com>
2022-02-02 18:00:33 +0900

    :alien: Update validation schema

--- src/rest/validaitons/updateTermValidation.js
+++ src/rest/validaitons/updateTermValidation.js
@@ -2,5 +2,5 @@ import * as yup from 'yup'
 
 export const updateTermSchema = yup.object().shape({
   description: yup.string().optional(),
-  tagNames: yup.array().of(yup.string().optional()).optional(),
+  termRelatedNames: yup.array().of(yup.string().optional()).optional(),
 })
